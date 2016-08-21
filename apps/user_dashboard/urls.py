2c9320cd2afdc635bb56fd32af8386acdbb6774e
Assuliya <t.nesterchuk@mail.ru>
2016-08-21 21:27:03 +0300

    models set. Login + registration. user session page vs other user page

--- apps/user_dashboard/urls.py
+++ apps/user_dashboard/urls.py
@@ -4,7 +4,13 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'user/login$', views.login, name='login'),
     url(r'user/register$', views.register, name='register'),
-    url(r'user/edit$', views.edit, name='edit'),
+    url(r'user/(?P<user_id>\d+)/edit$', views.edit, name='edit'),
     url(r'user/dashboard$', views.dashboard, name='dashboard'),
-    # url(r'delete/(?P<user_id>\d+)$', delete, name='log_delete')
+
+    url(r'^user/login_process$', views.login_process, name='login_process'),
+    url(r'^user/register_process$', views.register_process, name='register_process'),
+    url(r'^user/(?P<user_id>\d+)$', views.show, name='show'),
+    url(r'^user/logout$', views.logout, name='logout'),
+    url(r'^user/(?P<user_id>\d+)/delete$', views.delete, name='delete')
+
 ]
--- apps/user_dashboard/views.py
+++ apps/user_dashboard/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib import messages
-# from models import User
+from models import User, Message, Comment
 
 def index(request):
     return render(request, 'user_dashboard/index.html')
@@ -12,8 +12,51 @@ def login(request):
 def register(request):
     return render(request, 'user_dashboard/register.html')
 
-def edit(request):
+def edit(request, user_id):
     return render(request, 'user_dashboard/edit.html')
 
 def dashboard(request):
     return render(request, 'user_dashboard/dashboard.html')
+
+
+
+
+
+def register_process(request):
+    result = User.manager.validateReg(request)
+    if result[0] == False:
+        print_messages(request, result[1])
+        return redirect(reverse('register'))
+    return log_user_in(request, result[1])
+
+def login_process(request):
+    result = User.manager.validateLogin(request)
+    if result[0] == False:
+        print_messages(request, result[1])
+        return redirect(reverse('login'))
+    return log_user_in(request, result[1])
+
+def log_user_in(request, user):
+    request.session['user'] = user.id
+    return redirect(reverse('show', kwargs={'user_id':request.session['user']}))
+
+def show(request, user_id):
+    if not 'user' in request.session:
+        return redirect(reverse('index'))
+    user = User.manager.get(id=user_id)
+    context = {'user':user}
+    print (request.session['user'])
+    return render(request, 'user_dashboard/show.html', context)
+
+def print_messages(request, message_list):
+    for message in message_list:
+        messages.add_message(request, messages.ERROR, message)
+
+def logout(request):
+    request.session.pop('user')
+    return redirect(reverse('index'))
+
+def delete(request, user_id):
+    User.objects.delete(user_id)
+    request.session.pop('user')
+    return redirect('/')
